5912cc476bdea423447e65e36964fd6068f1f296
Alex M <milesAwayAlex@gmail.com>
2021-07-01 19:11:45 -0700

    Add an api for the flyover times

--- index.mjs
+++ index.mjs
@@ -3,11 +3,25 @@ import f from './fetch.mjs';
 const api = {
   ip: 'https://api.ipify.org/?format=json',
   geo: 'https://freegeoip.app/json/',
+  pass: ['http://api.open-notify.org/iss-pass.json?lat=', '&lon='],
 };
 
 f(api.ip, (err, ipObj) => {
   if (err) throw err;
   f(api.geo + ipObj.ip, (err, data) => {
-    console.log(err, data);
+    if (err) throw err;
+    f(
+      api.pass[0] + data.latitude + api.pass[1] + data.longitude,
+      (err, data) => {
+        if (err) throw err;
+        const passes = data.response.map(r => ({
+          dur: r.duration,
+          rise: r.risetime,
+        }));
+        passes.forEach(pass => {
+          console.log(pass);
+        });
+      }
+    );
   });
 });
